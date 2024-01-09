5273c35c2c6344b936611f8d44acd1a46ba8ada5
FangWanCai <2412119616@qq.com>
2024-01-09 18:14:48 +0800

    feat:'配置serve服务器'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    // 端口号
+    port: 8888,
+    // 主机名
+    host: 'localhost',
+    // 自动启动浏览器
+    open: true,
+    // http协议
+    // https: false,
+  },
+  // 关闭eslint
+  lintOnSave: false,
 });
